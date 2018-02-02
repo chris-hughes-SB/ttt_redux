0b3ea2b7431c2dfc997282745727b02b4fb09534
chris-hughes-SB <chris.hughes@simplybusiness.co.uk>
2018-02-02 12:44:02 +0000

    all into component files

---
+++ src/js/components/Square.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  render() {
+    return (
+      <button className="square">
+        {/* {fill this in} */}
+      </button>
+    );
+  }
+}
+
+export default Square;
